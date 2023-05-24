7d8d8008e36b5030eeba6ba20946b3ce40205421
Murillo <murillons11@gmail.com>
2023-05-24 12:23:44 -0300

    Formulario utilizando back end

---
+++ db/conn.js
@@ -0,0 +1,20 @@
+const {Sequelize} = require("sequelize")
+const sequelize = new Sequelize("banco_d","root","senai",{
+    host: "localhost",
+    dialect: "mysql"
+},{
+    createdAt:false,
+    updatedAt:false
+})
+
+// sequelize.authenticate().then(()=>{
+//     console.log("Conexão realizada com sucesso!")
+// }).catch((error)=>{
+//     console.log("Não é possivel conectart com banco de dados"+ error)
+// })
+
+
+
+module.exports = sequelize
+
+
